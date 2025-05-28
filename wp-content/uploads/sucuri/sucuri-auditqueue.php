d7957db052994ea1cea5421ce1d5ae9f85f3e197
infuginashish <infuginashish@gmail.com>
2025-05-28 17:07:23 +0530

    Pushing divikitz uploads c2 chunk 7

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1663566335;
+// updated_on=1663566335;
+exit(0);
+?>
+1663566335_9824:"Debug: admin@gobloxx.io, 2401:4900:30b9:e6dc:71a6:c23f:f797:e1a1; Sucuri plugin has been uninstalled"
---
+++ wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1663566335;
+// updated_on=1663566335;
+exit(0);
+?>
